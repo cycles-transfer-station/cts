a22d08503de384f75e966a12f78108a948a2517f
levi <levifeldman37@gmail.com>
2022-10-22 19:34:46 -0400

    build.sh with the cm_caller

--- rust/build.sh
+++ rust/build.sh
@@ -14,9 +14,9 @@ ic-wasm target/wasm32-unknown-unknown/release/cbs_map.wasm -o target/wasm32-unkn
 
 ic-wasm target/wasm32-unknown-unknown/release/cycles_bank.wasm -o target/wasm32-unknown-unknown/release/cycles_bank-o.wasm shrink
 
-ic-wasm target/wasm32-unknown-unknown/release/cycles_market.wasm -o target/wasm32-unknown-unknown/release/cycles_market-o.wasm shrink
-
 ic-wasm target/wasm32-unknown-unknown/release/cycles_transferrer.wasm -o target/wasm32-unknown-unknown/release/cycles_transferrer-o.wasm shrink
 
-ic-wasm target/wasm32-unknown-unknown/release/safe_caller.wasm -o target/wasm32-unknown-unknown/release/safe_caller-o.wasm shrink
+ic-wasm target/wasm32-unknown-unknown/release/cycles_market.wasm -o target/wasm32-unknown-unknown/release/cycles_market-o.wasm shrink
+
+ic-wasm target/wasm32-unknown-unknown/release/cm_caller.wasm -o target/wasm32-unknown-unknown/release/cm_caller-o.wasm shrink
 
