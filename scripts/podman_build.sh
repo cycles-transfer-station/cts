20970454b76f14d922fa6482569c1f056f231063
levi <levifeldman37@gmail.com>
2024-05-02 23:33:48 -0400

    use variable in podman_build

--- scripts/podman_build.sh
+++ scripts/podman_build.sh
@@ -10,7 +10,7 @@ container_id=$(podman create cts)
 podman cp $container_id:/cts/$BUILD_DIR $BUILD_DIR
 podman rm --volumes $container_id
 
-for file in `ls build`; 
+for file in `ls $BUILD_DIR`;
 do
-    sha256sum build/$file
+    sha256sum $BUILD_DIR/$file
 done
