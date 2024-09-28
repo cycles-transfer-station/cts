1775da2422472d8bfc0b570eba95d18bffae0f38
Levi. <levifeldman37@gmail.com>
2024-09-28 14:57:42 -0400

    space before module-hashes in podman_build.sh

--- scripts/podman_build.sh
+++ scripts/podman_build.sh
@@ -15,6 +15,8 @@ container_id=$(podman create cts)
 podman cp $container_id:/cts/$BUILD_DIR $BUILD_DIR
 podman rm --volumes $container_id
 
+printf "\n"
+
 for file in `ls $BUILD_DIR`;
 do
     sha256sum $BUILD_DIR/$file
