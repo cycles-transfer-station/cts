48034727601645697ca639bc54f2293eb0ec287a
Levi. <levifeldman37@gmail.com>
2024-09-29 16:43:06 -0400

    bank icrc3_certification.rs better naming

--- rust/canisters/bank/src/icrc3_certification.rs
+++ rust/canisters/bank/src/icrc3_certification.rs
@@ -2,8 +2,8 @@ use serde::Serialize;
 use serde_bytes::ByteBuf;
 
 
-const LABEL_LATEST_BLOCK_INDEX: &[u8; 16] = b"last_block_index";
-const LABEL_LATEST_BLOCK_HASH: &[u8; 15] = b"last_block_hash";
+const LABEL_LAST_BLOCK_INDEX: &[u8; 16] = b"last_block_index";
+const LABEL_LAST_BLOCK_HASH: &[u8; 15] = b"last_block_hash";
 
 use ic_cdk::api::set_certified_data;
 use ic_certified_map::{HashTree, fork_hash, labeled_hash, leaf_hash, fork, labeled};
@@ -16,8 +16,8 @@ pub fn set_root_hash(last_block_index: u64, last_block_hash: [u8; 32]) {
         v
     };
     let root_hash = fork_hash(
-        &labeled_hash(LABEL_LATEST_BLOCK_INDEX, &leaf_hash(&last_block_index_leb128[..])),
-        &labeled_hash(LABEL_LATEST_BLOCK_HASH, &leaf_hash(&last_block_hash[..])),
+        &labeled_hash(LABEL_LAST_BLOCK_INDEX, &leaf_hash(&last_block_index_leb128[..])),
+        &labeled_hash(LABEL_LAST_BLOCK_HASH, &leaf_hash(&last_block_hash[..])),
     );
     set_certified_data(&root_hash[..]);
 }
@@ -29,8 +29,8 @@ pub fn make_data_certificate_hash_tree(last_block_index: u64, last_block_hash: [
         v
     };
     let tree: HashTree = fork(
-        labeled(LABEL_LATEST_BLOCK_INDEX, HashTree::Leaf((&last_block_index_leb128[..]).into())),
-        labeled(LABEL_LATEST_BLOCK_HASH, HashTree::Leaf((&last_block_hash[..]).into())),
+        labeled(LABEL_LAST_BLOCK_INDEX, HashTree::Leaf((&last_block_index_leb128[..]).into())),
+        labeled(LABEL_LAST_BLOCK_HASH, HashTree::Leaf((&last_block_hash[..]).into())),
     );
     let mut serializer = serde_cbor::ser::Serializer::new(Vec::new());
     serializer.self_describe().unwrap();
